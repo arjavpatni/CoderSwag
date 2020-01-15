fa8400963fd0ae3e36edabf095a2a2ad82cae17a
Arjav <arjavpatni@hotmail.com>
2020-01-15 16:18:13 +0530

    Intent for Products Activity

--- app/src/main/java/com/arjavp/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/arjavp/coderswag/Controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arjavp.coderswag.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.arjavp.coderswag.Model.Category
 import com.arjavp.coderswag.R
 import com.arjavp.coderswag.Services.DataService
+import com.arjavp.coderswag.Utilities.EXTRA_CATEGORY
 import com.arjavp.coderswag.adapters.CategoryAdapter
 import com.arjavp.coderswag.adapters.CategoryRecycleAdapter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,7 +22,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryRecycleAdapter(this, DataService.categories) { category ->
-            
+            val productIntent = Intent(this, ProductsActivity :: class.java)
+            productIntent.putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
         }
         categoryListView.adapter = adapter
 
